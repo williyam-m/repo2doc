0aa9dd7438736332966836f0ed353f3aad5b0c7c
Williyam M <williyam64@gmail.com>
2025-07-10 19:27:22 +0530

    create load_qwen_model_from_transformer function

---
+++ ai_model/services/qwen_model.py
@@ -0,0 +1,21 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+import torch
+from admin_console.utils import ModelConfig
+
+generator = None
+
+def load_qwen_model_from_transformer():
+    global generator
+    if ModelConfig.isQwenFromTransformerEnabled() and generator is None:
+        tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen1.5-7B-Chat")
+        model = AutoModelForCausalLM.from_pretrained(
+            "Qwen/Qwen1.5-7B-Chat",
+            device_map="auto",
+            torch_dtype=torch.float16
+        )
+        generator = pipeline(
+            "text-generation",
+            model=model,
+            tokenizer=tokenizer,
+        )
+    return generator
